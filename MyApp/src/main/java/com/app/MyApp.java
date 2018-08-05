40027508d2b665f0e436f2156eca64ff352fcaaf
Admin <Admin@MyLappy>
2018-08-05 20:55:53 +0530

    First Commit

---
+++ MyApp/src/main/java/com/app/MyApp.java
@@ -0,0 +1,17 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SpringApplication.run(MyApp.class, args);
+		
+
+	}
+
+}
